183e3849fdd6cebe47b74f6b408b2d75be483b6e
nina <ninagbs@abv.bg>
2023-05-25 13:50:01 +0300

    Add uniqid library for id

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,10 +1,11 @@
+const uniqid = require('uniqid');
 const cubes = [];
 
 exports.getAll = ()=> cubes.slice();
 
 
 exports.create = (cubeData) => {
-    const newCube = {id: cubes.length+1,...cubeData}
+    const newCube = {id: uniqid(),...cubeData}
     cubes.push(newCube)
     return newCube;
 }
