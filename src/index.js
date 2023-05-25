e3a1f5de9f0b7c305343667039e32c9ebafc6848
nina <ninagbs@abv.bg>
2023-05-25 09:27:55 +0300

    Add handlebars to  project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
 const app = express();
 const PORT = 5000;
+
+// Handlebars config
+// refister the engine
+app.engine('hbs', handlebars.engine({
+    extname:'hbs'
+    
+}))
+// set engine
+app.set('view engine', 'hbs')
+// set view directory becouse by default it is in nmain directory
+app.set('views', 'src/views')
+
+
+// Routes
 app.get('/', (req,res)=>{
-    res.send('Hi from express');
+    res.render('index');
 })
-app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}...`))
+
+app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}...`));
