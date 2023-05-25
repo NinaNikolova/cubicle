66c4a5e7848dd6ae6d4d536a7e397434ef6fc048
nina <ninagbs@abv.bg>
2023-05-25 09:48:07 +0300

    add static assets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const handlebars = require('express-handlebars')
 const app = express();
+const path = require('path')
+
 const PORT = 5000;
+
+// express config -use - middleware express.static -access to css and img
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 // Handlebars config
 // refister the engine
