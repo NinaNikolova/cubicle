bb45cc30fdced7986c603825bdb58184b07618e0
nina <ninagbs@abv.bg>
2023-05-31 16:42:22 +0300

    Add accessory page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController')
 const cubeController =require('./controllers/cubeController')
+const accessoryController = require('./controllers/accessoryController')
 // Routes as middlewares -modular routes
 
 router.use(homeController)
 // work only if path req starts with '/cubes'
 router.use('/cubes', cubeController)
+router.use('/accessories', accessoryController)
 router.get("*", (req, res)=>{
     res.redirect(404)
 })
