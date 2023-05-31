c01171a19b912363724b1812e6dad292d602f871
nina <ninagbs@abv.bg>
2023-05-31 17:13:04 +0300

    add accessory/create page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -2,4 +2,10 @@ const router = require('express').Router();
 router.get('/create', (req, res)=>{
     res.render('accessory/create');
 })
+router.post('/create', (req, res)=>{
+    const {name, imageUrl, description}= req.body;
+    console.log(name, imageUrl, description)
+    // TODO: add accessory data to db
+    res.redirect('/')
+})
 module.exports=router;
