2ef56d35c835f82541f19314c34b80c69bd3fc26
nina <ninagbs@abv.bg>
2023-06-01 11:19:02 +0300

    Show remaining accessories only

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,3 +1,4 @@
 const Accessory = require('../models/Accessory')
 exports.create = (accessoryData) => Accessory.create(accessoryData);
 exports.getAll = () => Accessory.find();
+exports.getOthers = (accessoryIds) => Accessory.find({_id:{$nin: accessoryIds}})
