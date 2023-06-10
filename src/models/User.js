34d6b9addbb2dcdeeedced0e0a552008decb19bb
nina <ninagbs@abv.bg>
2023-06-10 19:26:17 +0300

    Save user with vaidated password

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    // heshiran password
+    password: String,
+
+})
+userSchema.virtual('repeatPassword')
+.set(function (value) {
+    if(value !== this.password){
+        throw new mongoose.MongooseError('Password missmatch');
+    }
+})
+const User = mongoose.model('User', userSchema);
+module.exports = User;
